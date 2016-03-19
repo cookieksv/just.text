8bc5b9c0a9a2e71ca547cee0432e3a44d5a69b10
cookie <cookie.ksv@gmail.com>
2016-03-19 21:27:42 +0200

    увеличил список жж

--- js/data.js
+++ js/data.js
@@ -52,16 +52,17 @@ var data = {
             article: 'article.b-singlepost-body',
             date: '.b-singlepost-author-userinfo-print time'
         }
-    }
-    /*gorkylook: {
-        name: 'gorkylook',
+    },
+    npubop: {
+        name: 'npubop',
         prefix: '',
         list: {
-            url: 'http://gorky-look.livejournal.com',
-            short: '.entry',
-            header: '.entry-title',
-            caption: '.entry-text',
-            link: '.entry-title>a'
+            url: 'http://npubop.livejournal.com',
+            skip: '/?skip=',
+            short: '.entryunit',
+            header: '.entryunit__title',
+            caption: '.entryunit__text',
+            link: '.entryunit__title>a'
         },
         page: {
             header: '.b-singlepost-title',
@@ -70,15 +71,16 @@ var data = {
             date: '.b-singlepost-author-userinfo-print time'
         }
     },
-    dolboeb: {
-        name: 'dolboeb',
+    bither: {
+        name: 'bither',
         prefix: '',
         list: {
-            url: 'http://dolboeb.livejournal.com/',
-            short: '.entryHolderBg > table',
-            header: '.entryHeader',
-            caption: '.entry-content',
-            link: '.entryHeader>a'
+            url: 'http://bither.livejournal.com',
+            skip: '/?skip=',
+            short: '.entryunit',
+            header: '.entryunit__title',
+            caption: '.entryunit__text',
+            link: '.entryunit__title>a'
         },
         page: {
             header: '.b-singlepost-title',
@@ -86,5 +88,114 @@ var data = {
             article: 'article.b-singlepost-body',
             date: '.b-singlepost-author-userinfo-print time'
         }
-    }*/
+    },
+    avmalgin: {
+        name: 'avmalgin',
+        prefix: '',
+        list: {
+            url: 'http://avmalgin.livejournal.com',
+            skip: '/?skip=',
+            short: '.j-e',
+            header: '.j-e-title',
+            caption: '.j-e-text',
+            link: '.j-e-title>a'
+        },
+        page: {
+            header: '.b-singlepost-title',
+            img: '.u-photo > img',
+            article: 'article.b-singlepost-body',
+            date: '.b-singlepost-author-userinfo-print time'
+        }
+    },
+    'a-s-k-e-t': {
+        name: 'a-s-k-e-t',
+        prefix: '',
+        list: {
+            url: 'http://a-s-k-e-t.livejournal.com',
+            skip: '/?skip=',
+            short: '.entryunit',
+            header: '.entryunit__title',
+            caption: '.entryunit__text',
+            link: '.entryunit__title>a'
+        },
+        page: {
+            header: '.b-singlepost-title',
+            img: '.u-photo > img',
+            article: 'article.b-singlepost-body',
+            date: '.b-singlepost-author-userinfo-print time'
+        }
+    },
+    horoshiyblog: {
+        name: 'horoshiyblog',
+        prefix: '',
+        list: {
+            url: 'http://horoshiyblog.livejournal.com',
+            skip: '/?skip=',
+            short: '.j-e',
+            header: '.j-e-title',
+            caption: '.j-e-text',
+            link: '.j-e-title>a'
+        },
+        page: {
+            header: '.j-e-title',
+            img: '.j-e-user-pic > img',
+            article: '.j-e-text',
+            date: '.j-e-date'
+        }
+    },
+    ibigdan: {
+        name: 'ibigdan',
+        prefix: '',
+        list: {
+            url: 'http://ibigdan.livejournal.com',
+            skip: '/?skip=',
+            short: '.entryunit',
+            header: '.entryunit__title',
+            caption: '.entryunit__text',
+            link: '.entryunit__title>a'
+        },
+        page: {
+            header: '.b-singlepost-title',
+            img: '.u-photo > img',
+            article: 'article.b-singlepost-body',
+            date: '.b-singlepost-author-userinfo-print time'
+        }
+    },
+    exler: {
+        name: 'exler',
+        prefix: '',
+        list: {
+            url: 'http://exler.livejournal.com',
+            skip: '/?skip=',
+            short: '.entryunit',
+            header: '.entryunit__title',
+            caption: '.entryunit__text',
+            link: '.entryunit__title>a'
+        },
+        page: {
+            header: '.b-singlepost-title',
+            img: '.u-photo > img',
+            article: 'article.b-singlepost-body',
+            date: '.b-singlepost-author-userinfo-print time'
+        }
+    },
+    varlamov: {
+        name: 'varlamov',
+        prefix: '',
+        list: {
+            url: 'http://varlamov.ru',
+            skip: '/?skip=',
+            short: '.j-e',
+            header: '.j-e-title',
+            caption: '.j-e-text',
+            link: '.j-e-title>a'
+        },
+        page: {
+            header: '.j-e-title',
+            img: '.j-e-user-pic > img',
+            article: '.j-e-text',
+            date: '.j-e-date'
+        }
+    }
+
 };
